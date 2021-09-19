c3600cb2b47265648a9107927f491f1fecbdb207
firja10 <68978827+firja10@users.noreply.github.com>
2021-09-19 18:38:31 +0700

    update arsanviella

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+    // Preloader
+    $(window).on("load", function () {
+        if ($("#preloader").length) {
+            $("#preloader")
+                .delay(100)
+                .fadeOut("slow", function () {
+                    $(this).remove();
+                });
+        }
+    });});
